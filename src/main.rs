a91b884389243d8ce15c30334274e12443da3205
bagjaehong <dkxltks25@naver.com>
2020-07-29 16:27:41 +0900

    rust chapter 2-1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!"); // 매크로
-    // println , ! -> 함수의 !(exclamation mark) 은 대개 매크로로 정의한다
-    // 러스트는 ahead-of-time compiled 언어이다.
-    // 소스코드없이 실행파일만 있어도 어디서든 실행이 가능하다.
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();  // mut -> mutable String :: new -> new는 String의 연관함수임을 나타낸다
+    io::stdin().read_line(&mut guess)
+       .expect("Failed to readLine");
 
 }
 
