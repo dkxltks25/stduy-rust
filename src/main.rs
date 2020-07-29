c8e561c5c3f5ea8a298d9372dda6ce953a7868ce
bagjaehong <dkxltks25@naver.com>
2020-07-29 16:03:30 +0900

    tutorial 1 end::

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!"); // 매크로
+    // println , ! -> 함수의 !(exclamation mark) 은 대개 매크로로 정의한다
+    // 러스트는 ahead-of-time compiled 언어이다.
+    // 소스코드없이 실행파일만 있어도 어디서든 실행이 가능하다.
+
 }
+
