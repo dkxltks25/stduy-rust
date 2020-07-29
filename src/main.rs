20b79419461b2df770f03cfe3b0c7f5119d8e7dd
bagjaehong <dkxltks25@naver.com>
2020-07-29 19:53:10 +0900

    rust chapter 3-2

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
-extern crate rand;
-
-// rand -> 외부에 의존하는 크레이트
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
-// use line 내에 선언되어야만 모든 스코프 내에서 사용 가능하다.
+// 8-bit	i8	u8
+// 16-bit	i16	u16
+// 32-bit	i32	u32
+// 64-bit	i64	u64
+// arch	isize	usize
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("check: {}", secret_number);
+    let f: bool = false;
+    let tup: (i32, f64, u8) = (500, 6.4, 1); // int 32 float 64 unsigned8
+    let (mut x, y, z) = tup;
 
-    loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-        let _guess: u32 =match guess.trim().parse()
-        {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
 
-        println!("You guessed:{}", _guess);
-        match _guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    println!("{}", f);
 }
