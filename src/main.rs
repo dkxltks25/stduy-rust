04121abcb2a69226bcc8a5ca2750f982cb05ee74
bagjaehong <dkxltks25@naver.com>
2020-07-30 15:43:09 +0900

    rust chapter 5-3

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,27 @@ struct Rectangle {
     width: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { length: 50, width: 30 };
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-
-    println!("rect1 is{:?}", rect1);
+// impl -> implementation 구현 블록
+impl Rectangle {
+    fn area(&self) -> u32 { // &self는 Reactangle의 구현체 내부에 있기에 Ractangle임을 알 수 있다.
+        // 만약 변경하기를 원한다면 &self가 아닌 &mut self가 되될 것이다
+        self.length * self.width
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
 }
 
-fn area(reactangle: &Rectangle) -> u32 {
-    reactangle.width * reactangle.length
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 45, width: 10 };
+    let rect3 = Rectangle { length: 40, width: 60 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect2? {}", rect2.can_hold(&rect2));
+    println!("rect is{:?}", Rectangle::square(4));
+    println!("rect1 is{:?}", rect1);
 }
