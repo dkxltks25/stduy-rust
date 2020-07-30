1c1cf6ff10ad14ed6076ed98d9bad6bbfef18f5f
bagjaehong <dkxltks25@naver.com>
2020-07-30 15:03:00 +0900

    rust chapter 5-1 struct sample

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
-    let s1 = gives_ownership(); // gives_onwsershipd은
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    let length1 = 50;
+    let width1 = 30;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(length1, width1)
+    );
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
-
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn area(length: u32, width: u32) -> u32 {
+    length * width
 }
